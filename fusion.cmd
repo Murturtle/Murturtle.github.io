04e7a110b5973f99f70642da9e6c34f900656dfa
Murturtle <97775022+Murturtle@users.noreply.github.com>
2023-04-24 22:00:30 -0700

    Update fusion.cmd

--- fusion.cmd
+++ fusion.cmd
@@ -1,24 +1,24 @@
 @echo off
 
 color f
-echo
+echo:
 echo [INFO] Using murturtle's Fusion360 jailbreak
-echo
+echo:
 echo [INFO] going into temp folder
 cd  "C:\Users\PhotonUser\My Files\Temporary Files"
 
 echo [KILL] killing fusion...
 taskkill /f /im Fusion360.exe
 echo [INFO] killed fusion
-echo
+echo:
 echo [KILL] killing Photon...
 taskkill /f /im PhotonWindowsCustomShell.exe
 taskkill /f /im PhotonWindowsAppSwitcher.exe
 taskkill /f /im PhotonWindowsCustomShellBackground.exe
 echo [INFO] killed photon
-echo
+echo:
 echo [INFO] you can now minimize windows
-echo
+echo:
 echo [INFO] proceding to download programs
 pause
 
@@ -30,9 +30,8 @@ goto finished
 
 :finished
 color a
-echo
+echo:
 echo [DONE] Setup complete! Press any key to open file explorer and cmd otherwise you will get locked out.
 pause
 start explorer "C:\Users\PhotonUser\My Files\Temporary Files"
 start cmd
-exit
