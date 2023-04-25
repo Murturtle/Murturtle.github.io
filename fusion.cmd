03d22dd554e027d6bc66da57306a06cdbe135ed6
Murturtle <97775022+Murturtle@users.noreply.github.com>
2023-04-24 22:21:11 -0700

    Update fusion.cmd

--- fusion.cmd
+++ fusion.cmd
@@ -8,12 +8,15 @@ echo [INFO] going into temp folder
 cd  "C:\Users\PhotonUser\My Files\Temporary Files"
 
 CHOICE /C YN /M "kill fusion?"
-IF %ERRORLEVEL% EQU 2 goto sub_hard_drive
-IF %ERRORLEVEL% EQU 1 continue
+IF %ERRORLEVEL% EQU 2 continue
+IF %ERRORLEVEL% EQU 1 goto fusion
 
+:fusion
 echo [KILL] killing fusion...
 taskkill /f /im Fusion360.exe
 echo [INFO] killed fusion
+continue
+
 echo:
 echo [KILL] killing Photon...
 taskkill /f /im PhotonWindowsCustomShell.exe
@@ -30,9 +33,7 @@ mkdir firefox
 xcopy "C:\Program Files (x86)\Mozilla Firefox" ".\firefox" /d
 cd firefox
 move firefox.exe a.exe
-goto finished
 
-:finished
 color a
 echo:
 echo [DONE] Setup complete! Press any key to open firefox and file explorer.
@@ -40,3 +41,4 @@ pause
 start explorer "C:\Users\PhotonUser\My Files\Temporary Files"
 cd firefox
 start a
+color
